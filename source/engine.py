06816f777f520858e9ae471b5276919d9f227e4b
HaKietHung2905 <hakiethung1998@gmail.com>
2024-09-18 11:32:28 +0700

    Update new dataset

--- source/engine.py
+++ source/engine.py
@@ -10,38 +10,42 @@ from coco_utils import get_coco_api_from_dataset
 
 
 def train_one_epoch(model, optimizer, data_loader, device, epoch, print_freq, scaler=None):
+    print("0")
     model.train()
     metric_logger = utils.MetricLogger(delimiter="  ")
     metric_logger.add_meter("lr", utils.SmoothedValue(window_size=1, fmt="{value:.6f}"))
     header = f"Epoch: [{epoch}]"
-
+    print("1")
     lr_scheduler = None
     if epoch == 0:
+        print("1.1")
         warmup_factor = 1.0 / 1000
         warmup_iters = min(1000, len(data_loader) - 1)
-
+        print("1.2")
         lr_scheduler = torch.optim.lr_scheduler.LinearLR(
             optimizer, start_factor=warmup_factor, total_iters=warmup_iters
         )
-
+        print("1.3")
+    print("2")
     for images, targets in metric_logger.log_every(data_loader, print_freq, header):
         images = list(image.to(device) for image in images)
         targets = [{k: v.to(device) if isinstance(v, torch.Tensor) else v for k, v in t.items()} for t in targets]
+        print("3")
         with torch.cuda.amp.autocast(enabled=scaler is not None):
             loss_dict = model(images, targets)
             losses = sum(loss for loss in loss_dict.values())
-
+        print("4")
         # reduce losses over all GPUs for logging purposes
         loss_dict_reduced = utils.reduce_dict(loss_dict)
         losses_reduced = sum(loss for loss in loss_dict_reduced.values())
 
         loss_value = losses_reduced.item()
-
+        print("5")
         if not math.isfinite(loss_value):
             print(f"Loss is {loss_value}, stopping training")
             print(loss_dict_reduced)
             sys.exit(1)
-
+        print("6")
         optimizer.zero_grad()
         if scaler is not None:
             scaler.scale(losses).backward()
@@ -53,7 +57,7 @@ def train_one_epoch(model, optimizer, data_loader, device, epoch, print_freq, sc
 
         if lr_scheduler is not None:
             lr_scheduler.step()
-
+        print("7")
         metric_logger.update(loss=losses_reduced, **loss_dict_reduced)
         metric_logger.update(lr=optimizer.param_groups[0]["lr"])
 
